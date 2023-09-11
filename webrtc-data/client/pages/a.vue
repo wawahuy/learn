4c6a0fd02d0a5aa2a3114e9c6128dfe37c2bf697
wawahuy <kakahuy99@gmail.com>
2023-09-11 20:42:24 +0700

    reup

---
+++ webrtc-data/client/pages/a.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>wtf {{ isOpen  }}</div>
+</template>
+
+<script setup lang="ts">
+import { io } from "socket.io-client";
+
+const nameMe = 'a';
+const isOpen = useState('isOpen', () => false);
+
+const socket = io("ws://localhost:8080");
+socket.on('connect', () => {
+    isOpen.value = true;
+    socket.emit('name', nameMe);
+})
+
+</script>
---
+++ webrtc-data/server/src/index.ts
@@ -0,0 +1,11 @@
+import { spawn } from 'child_process';
+const { RfbSession } = require('novnc-node');
+
+// const ps = spawn('java', [
+//     '-jar',
+//     '/home/yuh/microemulator-2.0.4/microemulator.jar'
+// ])
+
+// ps.stdout.on('data', (d) => {
+//     console.log(d);
+// })
